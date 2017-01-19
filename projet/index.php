b3cd6b6ac075a9ea821d0691082a0510e57dbc4e
KevinN21 <Kevin21.Nguyen@gmail.com>
2017-01-19 16:59:43 +0100

    MAJ v1.2

--- projet/index.php
+++ projet/index.php
@@ -22,20 +22,58 @@
         <meta name="twitter:image:src"         content="">
         <meta name="twitter:image"             content="">
 
-        <link rel="stylesheet" href="assets/css/global.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" >
+        <link rel="stylesheet" href="assets/css/global.css">
+        <!-- Latest compiled and minified CSS -->
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="assets/js/global.js"></script>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
       <title>Kevin Nguyen's Portfolio</title>
     </head>
   <body>
-    <header class="center">
-      <img src="assets/img/kevinn-portfolio-logo.svg" alt="Logo Kevin Nguyen Portfolio">
-      <h1>Web Developper</h1>
+    <header>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#"><i class="fa fa-home fa-fa4x" area-hidden="true"></i></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">Skill</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+      </nav>
+
+      <div class="container-fluid">
+        <img class="col-xs-12 col-md-4 col-md-offset-4" src="assets/img/kevinn-portfolio-logo.svg" alt="Logo Kevin Nguyen Portfolio">
+        <h1 class="col-xs-10 col-xs-offset-1 col-md-offset-1">Web Developper</h1>
+        <i class="col-xs-12 fa fa-arrow-down fa fa-3x" area-hidden="true"></i>
+      </div>
+      <div class="filler one"></div>
+      <nav id="navbar">
+        <ul>
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </ul>
+      </nav>
+      <div class="filler two"></div>
     </header>
-
-
-
   </body>
 </html>
