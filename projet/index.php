0a18c4f8079b8db016b339754c621d4df772baf5
KevinN21 <Kevin21.Nguyen@gmail.com>
2017-01-19 11:10:50 +0100

    Ajout et modif font logo css js

---
+++ projet/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta type="author" content="Kevin Nguyen" />
+        <meta type="description" content="Portfolio Kevin Nguyen" />
+        <meta name="keywords" content="Portfolio, Kevin Nguyen, Website, Work, Skill, Service, About me,HTML, CSS, XML, JavaScript">
+        <meta http-equiv="refresh" content="30">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+          <!-- META FACEBOOK  -->
+        <meta property="og:url"                content="" />
+        <meta property="og:type"               content="" />
+        <meta property="og:title"              content="" />
+        <meta property="og:description"        content="" />
+        <meta property="og:image"              content="" />
+        <!-- TWITTER -->
+        <meta name="twitter:card"              content="" />
+        <meta name="twitter:creator"           content="">
+        <meta name="twitter:title"             content="">
+        <meta name="twitter:description"       content="">
+        <meta name="twitter:image:src"         content="">
+        <meta name="twitter:image"             content="">
+
+        <link rel="stylesheet" href="assets/css/global.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" >
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <title>Kevin Nguyen's Portfolio</title>
+    </head>
+  <body>
+    <header class="center">
+      <img src="assets/img/kevinn-portfolio-logo.svg" alt="Logo Kevin Nguyen Portfolio">
+      <h1>Web Developper</h1>
+    </header>
+
+
+
+  </body>
+</html>
