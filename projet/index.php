cf0d234272bb47a4bd361531daa2e33fa1789079
KevinN21 <Kevin21.Nguyen@gmail.com>
2017-01-26 16:56:54 +0100

    portofolio maj 1.2

--- projet/index.php
+++ projet/index.php
@@ -5,15 +5,15 @@
         <meta type="author" content="Kevin Nguyen" />
         <meta type="description" content="Portfolio Kevin Nguyen" />
         <meta name="keywords" content="Portfolio, Kevin Nguyen, Website, Work, Skill, Service, About me,HTML, CSS, XML, JavaScript">
-        <meta http-equiv="refresh" content="30">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-          <!-- META FACEBOOK  -->
+        <!-- META FACEBOOK  -->
         <meta property="og:url"                content="" />
         <meta property="og:type"               content="" />
         <meta property="og:title"              content="" />
         <meta property="og:description"        content="" />
-        <meta property="og:image"              content="" />
+        <meta property="og:image"              content="1200*630" />
         <!-- TWITTER -->
         <meta name="twitter:card"              content="" />
         <meta name="twitter:creator"           content="">
@@ -24,7 +24,13 @@
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" >
-        <link rel="stylesheet" href="assets/css/global.css">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <!-- <link rel="stylesheet" href="assets/css/global.css"> -->
         <!-- Latest compiled and minified CSS -->
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -33,13 +39,13 @@
 
       <title>Kevin Nguyen's Portfolio</title>
     </head>
-  <body>
-    <header>
+
+<body>
+<header>
       <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <div  class="container-fluid">
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -48,32 +54,247 @@
               <a class="navbar-brand" href="#"><i class="fa fa-home fa-fa4x" area-hidden="true"></i></a>
             </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
                 <li><a href="#">About</a></li>
                 <li><a href="#">Work</a></li>
                 <li><a href="#">Skill</a></li>
                 <li><a href="#">Contact</a></li>
               </ul>
-            </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-      </nav>
+            </div>
+       </div>
+  </nav>
+  <div class="row">
+    <img class="col-xs-11 col-md-4" src="assets/img/kevinn-portfolio-logo.svg" alt="Logo Kevin Nguyen Portfolio">
+    <h1 class="col-xs-10">Web Developper</h1>
+    <i class="col-xs-12 fa fa-arrow-down fa fa-3x"></i>
+  </div>
 
-      <div class="container-fluid">
-        <img class="col-xs-12 col-md-4 col-md-offset-4" src="assets/img/kevinn-portfolio-logo.svg" alt="Logo Kevin Nguyen Portfolio">
-        <h1 class="col-xs-10 col-xs-offset-1 col-md-offset-1">Web Developper</h1>
-        <i class="col-xs-12 fa fa-arrow-down fa fa-3x" area-hidden="true"></i>
+</header>
+
+  <main class="container-fluid">
+    <section id="k-abtme"    class="row">
+        <article class="col-xs-12">
+          <h2 >About me</h2>
+          <figure class="row">
+            <figcaption class="col-md-12 col-xs12">
+                <img class="col-md-3 col-xs-12" src="assets/img/kevin.jpg" alt="Ma tête Kevin Nguyen" width="300px" height="300px">
+                <h2>Kevin Nguyen 21 ans</h2>
+                <h3>Developper Web</h3>
+                <a href="https://github.com/KevinN21"><img src="assets/img/github.svg" target='_blank' alt="Personal GitHub Kevin Nguyen"></a>
+                <a href="https://www.linkedin.com/in/kevin21-nguyen"><img src="assets/img/linkedin.svg" target='_blank' alt="Personal Linkedin Kevin Nguyen"></a>
+                <p class="col-md-5 col-xs-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio. Lorem </p>
+            </figcaption>
+        </article>
+            <section class="container">
+              <article class="row">
+                <p class="col-md-12 col-xs-10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                Lorem
+                </p>
+              </article>
+              <article class="col-md-6 col-xs-12">
+                <img class="col-md-2" src="assets/img/language.svg" alt="Petit logo">
+                <h4 class="col-md-8">Language / Langue / Ideomas</h4>
+                <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                Lorem</p>
+              </article>
+              <article class="col-md-6 col-xs-12">
+                  <img class="col-md-2" src="assets/img/language.svg" alt="Petit logo">
+                  <h4 class="col-md-8">Language / Langue / Ideomas</h4>
+                  <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                  Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                  Lorem</p>
+              </article>
+              <article class="col-md-6 col-xs-12">
+                <img class="col-md-2" src="assets/img/language.svg" alt="Petit logo">
+                <h4 class="col-md-8">Language / Langue / Ideomas</h4>
+                <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                Lorem</p>
+              </article>
+              <article class="col-md-6 col-xs-12">
+                <img class="col-md-2" src="assets/img/language.svg" alt="Petit logo">
+                <h4 class="col-md-8">Language / Langue / Ideomas</h4>
+                <p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+                Lorem</p>
+              </article>
+            </section>
+    </section>
+    <section id="k-works"    class="row">
+      <article class="row">
+        <h2>My Work</h2>
+      </article>
+      <figure class="row">
+        <figcaption class="col-md-4">
+          <img src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+        </figcaption>
+        <figcaption class="col-md-4">
+          <img src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+        </figcaption>
+        <figcaption class="col-md-4">
+          <img src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+        </figcaption>
+      </figure>
+      <figure class="row">
+        <figcaption class="col-md-4">
+          <img src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+        </figcaption>
+        <figcaption class="col-md-4">
+          <img src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+        </figcaption>
+        <figcaption class="col-md-4">
+          <img src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+        </figcaption>
+      </figure>
+      <article class="row">
+        <h3>Description du projet :</h3><p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+        Lorem Lorem ipsum dolor sit amet, consecm </p>
+        <h3>Date du projet :</h3><p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+        Lorem Lorem ipsum dolor sit amet, consecrem </p>
+        <h3>Lien du projet :</h3><p class="col-md-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut condimentum dui, ut ultrices turpis. Vivamus erat odio.
+        Lorem Lorem ipsum dolor sit amet, consec </p>
+      </article>
+      <article class="row">
+        <h2>My Showcase</h2>
+
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img class="d-block img-fluid" src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+          </div>
+
+          <div class="item">
+            <img class="d-block img-fluid" src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+          </div>
+
+          <div class="item">
+            <img class="d-block img-fluid" src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+          </div>
+
+          <div class="item">
+            <img class="d-block img-fluid" src="assets/img/kebab.jpg" alt="My work" style="width:100%;">
+          </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
-      <div class="filler one"></div>
-      <nav id="navbar">
-        <ul>
-          <li>One</li>
-          <li>Two</li>
-          <li>Three</li>
-        </ul>
-      </nav>
-      <div class="filler two"></div>
-    </header>
-  </body>
+      </article>
+      <script type="text/javascript">
+      $('.carousel').carousel()
+      </script>
+    </section>
+    <section id="k-skills"   class="row">
+      <article class="col-md-12">
+        <h2>Technical Skills</h2>*
+        <div class="row">
+          <div class="col-md-6">
+            <h4 class="col-md-6">Programmation</h4>
+              <figcaption class="col-md-12">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+              </figcaption>
+          </div>
+          <div class="col-md-6">
+            <h4 class="col-md-6">Graphisme</h4>
+              <figcaption class="col-md-12">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+              </figcaption>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <h4 class="col-md-6">Framework</h4>
+              <figcaption class="col-md-12">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+              </figcaption>
+          </div>
+          <div class="col-md-6">
+            <h4 class="col-md-6">Database</h4>
+              <figcaption class="col-md-12">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+              </figcaption>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <h4 class="col-md-6">CMS</h4>
+              <figcaption class="col-md-12">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+              </figcaption>
+          </div>
+          <div class="col-md-6">
+            <h4 class="col-md-6">Other</h4>
+              <figcaption class="col-md-12">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+                <img src="assets/img/kebab.jpg" alt="" style="width:20%;">
+              </figcaption>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section id="k-contact"  class="row">
+      <article class="col-md-12">
+        <div class="row">
+          <h2>Contact me</h2>
+        </div>
+      </article>
+        <form class="form-group" action="" method="post" novalidate="">
+          <div class=" col-md-6">
+            <label for="name">Firstname Lastname*</label>
+            <input name="name" type="text" class="form-control" id="" placeholder="First name & Last name">
+          </div>
+          <div class="col-md-6">
+              <label for="email">Email*</label>
+              <input name="email" type="text" class="form-control" id="" placeholder="Email">
+          </div>
+          <div class="col-md-12">
+              <label for="message">Message</label>
+              <textarea name="message" class="form-control" rows="8" cols="80" placeholder="Write your message"></textarea>
+              <button class="btn btn-default" type="submit" name="button">Send </button>
+          </div>
+        </form>
+    </section>
+  </main>
+<footer id="k-foot" class="row">
+  <nav>
+    <ul>
+      <li>Kevin Nguyen © - 2017</li>
+      <li><i class="fa fa-envelope"></i> kevin21.nguyen@gmail.com for business inquieris</li>
+      <li><i class="fa fa-phone"></i> +33 6 70 31 77 85</li>
+    </ul>
+  </nav>
+</footer>
+</body>
 </html>
